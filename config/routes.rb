6df908775a32466ad05012c0944a1460057ce1f7
Amanda <amandaben21@aol.com>
2021-03-26 12:22:14 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,19 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  #get '/admin/stats', to: 'stats#index' 
 
+  #scope '/admin', module: 'admin' do  -->       #we write scope because we want to use /admin as url prefix and
+                                              # also letting Rails know that all of the included routes will be handle by controllers in the admin module
+    #resources :stats, only: [:index]
+  #end 
+  # is the same as | 
+  #                v
+  
+  namespace :admin do               #namespace is a shorter way as i see it :]
+    resources :stats, only: [:index]
+  end 
+  
   root 'posts#index'
+
 end
